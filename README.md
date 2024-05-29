0358ec68cedc3fce565e85d9826b1c515cb61e49
Jake Walker <hi@jakew.me>
2024-05-29 17:45:22 +0100

    add readme, comments and license

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
 # Tildagon Bar App
 
-> I've only tested this on a simulator so far, so not sure if it works on real hardware yet!
+> This is a work in progress and doesn't work very well yet. I've only tested this on a simulator so far, so not sure if it works on real hardware yet!
 
-A [Tildagon Badge](https://tildagon.badge.emfcamp.org/) app for browsing drinks available at the bars at [EMF](https://emfcamp.org).
+A [Tildagon Badge](https://tildagon.badge.emfcamp.org/) app for browsing drinks available and their stock level at [EMF](https://emfcamp.org) bars.
 
-This is a work in progress and contributions are more than welcome!
+## Todo
+
+- [ ] Implement bar switching
+- [ ] Make the drink details screen look nicer (perhaps not a menu)
+- [ ] Cache data to a file for faster loading
+- [ ] Show more info on the drink selection menu?
 
 ## Development
 
-Start a local fake API:
+The live development API didn't work for me, so here's a _very_ simple mock API to work with:
 
 ```python
 cd fake_api
 python -m http.server
 ```
+
+## Contributing
+
+Any pull requests are welcome!
+
+## License
+
+[The Unlicense](https://unlicense.org/)
--- main.py
+++ main.py
@@ -8,11 +8,16 @@ from events.input import Buttons
 class BarApp(app.App):
     data = {}
 
+    # what bar to get data for - main/cybar
     bar = "main"
+    # what drink category - first level of data keys
     category = None
+    # what drink index within the category
     drink_index = None
 
+    # font size for main menu
     main_menu_font_size = 12
+    # font size for drink and drink details menus
     sub_menu_font_size = 6
 
     error = None
@@ -28,17 +33,22 @@ class BarApp(app.App):
         self._refresh_data()
 
     def update_menu(self):
+        # this is set to zero to prevent it being larger than the number of items in the list
         self.menu.position = 0
 
+        # set the menu style
         if self.category is None:
+            # main menu
             self.menu.item_font_size = self.main_menu_font_size * one_pt
             self.menu.focused_item_font_size = (self.main_menu_font_size + 4) * one_pt
             self.menu.item_line_height = self.menu.item_font_size * line_height
         else:
+            # drinks menu or drink details
             self.menu.item_font_size = self.sub_menu_font_size * one_pt
             self.menu.focused_item_font_size = (self.sub_menu_font_size + 4) * one_pt
             self.menu.item_line_height = self.menu.item_font_size * 1.1
 
+        # set the menu items for a selected drink
         if self.drink_index is not None:
             drink = self.data[self.category][self.drink_index]
             self.menu.menu_items = [
@@ -49,16 +59,19 @@ class BarApp(app.App):
             ]
             return
 
+        # set the menu items for the main menu
         if self.category is None:
             self.menu.menu_items = [
                 category.title() for category in self.data.keys()
             ] + [f"Bar: {self.bar}", "Refresh"]
             return
 
+        # set the menu item for if there's no drinks in the category
         if len(self.data[self.category]) == 0:
             self.menu.menu_items = ["Nothing here :("]
             return
 
+        # set the menu items for drinks in the category
         self.menu.menu_items = list(
             [
                 (item["stocktype"]["manufacturer"] + " " + item["stocktype"]["name"])
@@ -68,6 +81,7 @@ class BarApp(app.App):
 
     def select_handler(self, item):
         if self.category is None:
+            # main menu
             if item.startswith("Bar:"):
                 if self.bar == "main":
                     self.bar = "cybar"
@@ -80,6 +94,7 @@ class BarApp(app.App):
                 self._refresh_data()
                 return
 
+            # convert the item back to lower (as it is converted to title when displayed)
             self.category = item.lower()
         elif self.drink_index is None:
             self.drink_index = self.menu.position
@@ -88,12 +103,15 @@ class BarApp(app.App):
 
     def back_handler(self):
         if self.drink_index is not None:
+            # if in drink details, unsetting the drink id takes back to the drinks list
             self.drink_index = None
             self.update_menu()
         elif self.category is not None:
+            # if in drinks list, unsetting the category goes back to the main menu
             self.category = None
             self.update_menu()
         else:
+            # if at the main menu, quit the app
             self.minimise()
 
     def draw(self, ctx):
@@ -106,8 +124,10 @@ class BarApp(app.App):
     def _refresh_data(self):
         try:
             bar_json = urequests.get("http://localhost:8000/api/on-tap.json")
+            # todo: confirm ujson is on the badge
             self.data = ujson.loads(bar_json.text)
-        except:
+        except:  # noqa
+            # todo: show this error message somewhere
             self.error = "Could not get bar data"
             self.data = {}
 
